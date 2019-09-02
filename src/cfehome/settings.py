016a62a20660a2f639fee957a02c72978647cbae
mr-star-k <pramodjaiswal517@gmail.com>
2019-09-02 23:35:09 +0530

    redis implemented and msg echo done

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -43,6 +43,15 @@ INSTALLED_APPS = [
 
 ASGI_APPLICATION = "cfehome.routing.application"
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("localhost", 6379)],
+        },
+    },
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -13,9 +13,33 @@ class ChatConsumer(AsyncConsumer):
         await self.send({
             "type":"websocket.accept"
         })
-    
+        other_user = self.scope['url_route']['kwargs']['username']
+        me = self.scope['user']
+        print(other_user,me)
+        thread_obj = await self.get_thread(me,other_user)
+        
     async def websocket_receive(self,event):
         print("receive", event)
+        front_text = event.get('text',None)
+        if front_text is not None:
+            loaded_dict_data = json.loads(front_text)
+            msg = loaded_dict_data.get('message')
+            user = self.scope['user']
+            username = 'default'
+            if user.is_authenticated:
+                username = user.username
+            myResponse = {
+                'message':msg,
+                'username': username
+            }
+            await self.send({
+                "type":"websocket.send",
+                "text":json.dumps(myResponse)
+            })
     
     async def websocket_disconnect(self,event):
         print("disconnected", event)
+    
+    @database_sync_to_async
+    def get_thread(self,user, other_username):
+        return Thread.objects.get_or_new(user,other_username)[0]
